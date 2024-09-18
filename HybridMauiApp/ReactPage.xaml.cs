19b5a6a86872e57b5c2f2d12d8a5f68dfbd62cd7
Beth Massi <bethma@microsoft.com>
2024-09-18 13:37:22 -0700

    Add project files.

---
+++ HybridMauiApp/ReactPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using MauiReactJSHybridApp;
+using System.Text.Json.Serialization;
+
+namespace HybridMauiApp
+{
+    public partial class ReactPage : ContentPage
+    {
+        private readonly TodoDataStore _todoDataStore;
+        public ReactPage()
+        {
+            InitializeComponent();
+
+            _todoDataStore = new TodoDataStore();
+            _todoDataStore.TaskDataChanged += OnTodoDataChanged;            
+
+            BindingContext = this;
+        }
+
+        public string TodoAppTitle => $"Todo items: {_todoDataStore.GetData().Count}";
+
+        private void OnTodoDataChanged(object? sender, EventArgs e)
+        {
+            OnPropertyChanged(nameof(TodoAppTitle));
+        }
+
+
+
+    }
+
+}
